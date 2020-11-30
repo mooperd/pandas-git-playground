f9c79c5a4dcfe647d3b76e9666db0762be078fc2
Andrew Holway <andrew.holway@gmail.com>
2020-11-30 10:44:51 +0100

    added the rest of the files

---
+++ git-log-pitar.py
@@ -0,0 +1,30 @@
+import pandas as pd
+pd.set_option('display.max_columns', None) 
+pd.set_option('display.max_rows', None)
+dataframe = pd.read_csv("git-log-2020.csv")
+dataframe['date'] = pd.to_datetime(dataframe['date'])
+
+top_three = dataframe.groupby(pd.Grouper(key='date', freq='1M')).apply(lambda d: d['email'].value_counts().sort_values(ascending = False).head(3))
+
+
+print(top_three)
+
+    
+# 
+
+# dataframe.groupby(pd.Grouper(key='date', freq='M'))['email'].value_counts().sort_values(ascending = False).head(3))
+
+
+# grouped_dataframe_count = dataframe.groupby([pd.Grouper(key='date', freq='M'), "email"])[["subject"]].count()
+# grouped_dataframe_agg = dataframe.groupby([pd.Grouper(key='date', freq='M'), "email"])[["subject"]].agg(['count'])
+
+# grouped_dataframe_count.groupby(pd.Grouper(key='date', freq='M'))['email'].value_counts().sort_values(ascending = False).head(3)
+
+#print(grouped_dataframe_count["subject"].transform(max))
+# Select all the users that have contributed more than 20 times.
+# print(grouped_dataframe_count[grouped_dataframe_count['subject'] > 20])
+
+
+
+# grouped_dataframe_agg = dataframe.groupby([pd.Grouper(key='date', freq='M'), "email"]).agg({'subject': {lambda x: x.nlargest(2).count() }})
+
---
+++ scratch.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+pd.set_option('display.max_columns', None) 
+pd.set_option('display.max_rows', None)
+dataframe = pd.read_csv("git-log-2020.csv")
+dataframe['date'] = pd.to_datetime(dataframe['date'])
+
+def my_function(row):
+    print("hello")
+    return row['email'].value_counts().sort_values(ascending = False).head(3)
+
+top_three = dataframe.groupby(pd.Grouper(key='date', freq='1M')).apply(my_function)
+
+print(top_three)
+
+fig, ax = plt.subplots(figsize=(6, 10))
+top_three.unstack().plot(kind='bar', ax=ax, stacked=True)
+plt.show()
